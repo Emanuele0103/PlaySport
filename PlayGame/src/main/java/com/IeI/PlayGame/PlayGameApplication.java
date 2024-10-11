75073a0c043d06ebd89d278b0b8e17f40b0b9f0e
Emanuele Grande <emanuelegrande2@gmail.com>
2024-10-11 17:06:08 +0200

    Create Spring Project

---
+++ PlayGame/src/main/java/com/IeI/PlayGame/PlayGameApplication.java
@@ -0,0 +1,13 @@
+package com.IeI.PlayGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayGameApplication.class, args);
+	}
+
+}
