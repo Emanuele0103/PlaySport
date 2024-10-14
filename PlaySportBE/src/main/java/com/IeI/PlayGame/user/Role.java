dc72069e2d6256c43052f366953e4a78da96e19e
Emanuele Grande <emanuelegrande2@gmail.com>
2024-10-14 10:58:42 +0200

    Create User Entity

---
+++ PlaySportBE/src/main/java/com/IeI/PlayGame/user/Role.java
@@ -0,0 +1,6 @@
+package com.IeI.PlayGame.user;
+
+public enum Role {
+    USER,
+    ADMIN;
+}
---
+++ PlaySportBE/src/main/java/com/IeI/PlayGame/user/User.java
@@ -0,0 +1,71 @@
+package com.IeI.PlayGame.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "_user_")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column
+    private String firstname;
+    @Column
+    private String lastname;
+    @Column
+    private String email;
+    @Column
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
